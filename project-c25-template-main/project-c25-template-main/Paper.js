d98f5bae9bd7d6e66ec97b86980979d61fea7133
kvssff <77674259+kvssff@users.noreply.github.com>
2021-04-21 17:25:31 +0530

    Add files via upload

---
+++ project-c25-template-main/project-c25-template-main/Paper.js
@@ -0,0 +1,27 @@
+class Paper {
+ constructor(x,y,r) {  
+ var options={
+     istatic:false,
+ restitution:0.1,
+ friction:0,
+ density:2  
+ }    
+this.x=x;
+this.y=y;
+this.r=r;
+
+ this.image=loadImage("paper.png")
+this.body=Bodies.circle(this.x,this.y,this.r,options)
+World.add(world,this.body);
+}
+
+display(){
+ var pos=this.body.position;   
+push();
+translate(pos.x,pos.y)
+imageMode(CENTER);
+image(this.image,0,0,this.r,this.r)
+ pop();
+
+}
+}
